9c373cd8d89c0920b22cfe6854f4dd08afa27284
Atharva Sharma <mailatharvasharma@gmail.com>
2022-02-13 22:47:27 +0530

    feat: tailwind typography example

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial />
+  <article class="p-6 mx-auto prose prose-headings:font-light md:prose-lg lg:prose-xl max-w-4xl">
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+  </article>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  async asyncData ({ $content }) {
+    const page = await $content('home').fetch()
+    return { page }
+  }
 }
 </script>
